049effb3c35459f48e632b111bd7a20b9631dd76
Alecto <fomenkoandrey1978@gmail.com>
2024-11-16 16:52:16 +0200

    оновлено тести, оновлено код, додано пояснення

--- src/main.js
+++ src/main.js
@@ -12,8 +12,20 @@ function isValidEmail(email) {
   // code here
 }
 
+// Тестові випадки
 // console.log(isValidEmail('example@example.com')) // Повинно вивести: true
-// console.log(isValidEmail('invalid-email'))       // Повинно вивести: false
+// console.log(isValidEmail('example.com'))        // Повинно вивести: false (немає @)
+// console.log(isValidEmail('example@'))           // Повинно вивести: false (немає домену)
+// console.log(isValidEmail('example@example.abcdef')) // Повинно вивести: false (довгий TLD)
+// console.log(isValidEmail('mail@.mail.com.ua'))  // Повинно вивести: false (крапка на початку домену)
+// console.log(isValidEmail(''))                   // Повинно вивести: false (пустий рядок)
+// console.log(isValidEmail(undefined))            // Повинно вивести: false (undefined)
+// console.log(isValidEmail('user@sub.domain.com')) // Повинно вивести: true (коректна адреса)
+// console.log(isValidEmail('u.ser+123@domain.org')) // Повинно вивести: true (допустимі символи)
+// console.log(isValidEmail('example@domain.c'))   // Повинно вивести: false (TLD занадто короткий)
+// console.log(isValidEmail('example@-domain.com')) // Повинно вивести: false (дефіс на початку домену)
+// console.log(isValidEmail('example@domain..com')) // Повинно вивести: false (подвійна крапка в домені)
+// console.log(isValidEmail('.example@domain.com')) // Повинно вивести: false (крапка на початку локальної частини)
 
 /*
  * #2
